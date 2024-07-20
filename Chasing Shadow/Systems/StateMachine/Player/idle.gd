705ebfc6d51c9673589670abf600071d33a49913
Jon Fingas <jon.fingas@gmail.com>
2024-07-20 14:03:14 -0500

    modification to idle state to prevent dead button pressing

--- Chasing Shadow/Systems/StateMachine/Player/idle.gd
+++ Chasing Shadow/Systems/StateMachine/Player/idle.gd
@@ -30,7 +30,11 @@ func process_input(_event: InputEvent) -> State:
 		return walk
 	if Input.is_action_just_pressed("jump"):
 		return jump
-	if Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_right"):
+	## checking if the key is pressed and returning to run state if pressed,
+	## this is to combat pressting move_left and move right at the same time and then letting go
+	## of one and not move at all until you let go of both.
+	## needs more testing.
+	if Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"):
 		return run
 
 	return null
