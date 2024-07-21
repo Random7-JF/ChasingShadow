8bcf9a3f5e8b824540adce9f2fb34f25077a7fa7
Jon Fingas <jon.fingas@gmail.com>
2024-07-21 02:43:59 -0500

    got a buggy 3 stage attack made

--- Chasing Shadow/Scenes/Characters/Player/player.gd
+++ Chasing Shadow/Scenes/Characters/Player/player.gd
@@ -4,15 +4,19 @@ extends CharacterBody2D
 @onready var state_machine: StateMachine = $StateMachine
 @onready var animator: AnimationPlayer = $AnimationPlayer
 
-@export var jump_velocity: float = 400
+
 @export var speed: float = 500
 @export var wall_grip: float = 0.50
-
 var can_wall_slide: bool = true
+
+@export var jump_velocity: float = 400
 var can_double_jump: bool = true
 
 var attack_can_combo: bool = false
 var attack_combo: int = 0
+var attacking: bool = false
+var attack_time_since: float = 0
+@export var attack_delay: float = 0.3
 
 func _ready():
 	state_machine.init(self, animator)
--- Chasing Shadow/Scenes/UI/Debug/attack.gd
+++ Chasing Shadow/Scenes/UI/Debug/attack.gd
@@ -6,4 +6,5 @@ extends Label
 func _process(_delta):
 	if player:
 		text = "Attack Combo: " + str(player.attack_can_combo) + "
-		Attack # " + str(player.attack_combo)
+		Attack # " + str(player.attack_combo) + "
+		Attacking: " + str(player.attacking)
